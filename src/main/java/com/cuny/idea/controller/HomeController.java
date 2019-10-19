c037e6ad12bff4971cd1fc1c2b0dbed452db78d7
Freakaaron <arpitindersingh805@gmail.com>
2019-10-19 18:56:25 -0400

    build able product

--- src/main/java/com/cuny/idea/controller/HomeController.java
+++ src/main/java/com/cuny/idea/controller/HomeController.java
@@ -4,6 +4,8 @@ import com.cuny.idea.bean.Idea;
 import com.cuny.idea.repository.IdeaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -12,6 +14,11 @@ public class HomeController {
 
     @Autowired
     private IdeaRepository ideaRepository;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView fetch() {
+        return new ModelAndView(new RedirectView("index.html"));
+    }
 
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     @ResponseBody
